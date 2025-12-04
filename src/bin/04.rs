d44b5df9e51fc981466c26bbdedfe040289f7689
Stijn Veenman <stijn.veenman@schiphol.nl>
2025-12-04 08:11:55 +0100

    d4p1

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,15 +1,27 @@
 advent_of_code::solution!(4);
+use advent_of_code::grid::{Grid, char_grid::CharGrid};
 #[allow(unused_imports)]
 use advent_of_code::prelude::*;
 
-fn parse_input(input: &str) {
-    // TODO: parse input
+fn parse_input(input: &str) -> advent_of_code::grid::char_grid::CharGrid {
+    CharGrid::new(input)
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let input = parse_input(input);
+    let grid = parse_input(input);
 
-    None
+    Some(
+        grid.keys()
+            .filter(|p| grid.get(p).is_some_and(|c| c == '@'))
+            .map(|p| {
+                p.full_neighbours()
+                    .iter()
+                    .filter(|n| grid.get(n).is_some_and(|c| c == '@'))
+                    .count()
+            })
+            .filter(|count| *count < 4)
+            .count() as u64,
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
