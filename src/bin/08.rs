d18bf8c767f42504c4aa95f43b889eb012fb0490
Stijn Veenman <stijn.veenman@schiphol.nl>
2025-12-08 18:17:41 +0100

    d8p2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,9 +1,6 @@
 advent_of_code::solution!(8);
 
-use std::{
-    collections::{BTreeMap, HashMap, HashSet, btree_map::Entry},
-    ops::AddAssign,
-};
+use std::collections::{BTreeMap, HashMap, btree_map::Entry};
 
 #[allow(unused_imports)]
 use advent_of_code::prelude::*;
@@ -104,6 +101,61 @@ pub fn part_one(input: &str) -> Option<u64> {
 pub fn part_two(input: &str) -> Option<u64> {
     let input = parse_input(input);
 
+    let mut distances = BTreeMap::new();
+    let mut idx = 0;
+    let mut circuits: HashMap<&[i64; 3], u64> = HashMap::new();
+
+    for (left, right) in input.iter().tuple_combinations() {
+        let distance: i64 = left
+            .iter()
+            .zip(right.iter())
+            .map(|(p1, p2)| (p2 - p1).pow(2))
+            .sum::<i64>()
+            .isqrt();
+
+        match distances.entry(distance) {
+            Entry::Vacant(entry) => {
+                entry.insert(vec![(left, right)]);
+            }
+            Entry::Occupied(mut entry) => {
+                entry.get_mut().push((left, right));
+            }
+        }
+    }
+
+    for (left, right) in distances.values().flatten() {
+        let left_circuit = circuits.get(left);
+        let right_circuit = circuits.get(right);
+
+        match (left_circuit, right_circuit) {
+            (Some(left_circuit), Some(right_circuit)) if left_circuit == right_circuit => {}
+            (Some(left_circuit), Some(right_circuit)) => {
+                let left_circuit = *left_circuit;
+                let right_circuit = *right_circuit;
+                circuits.iter_mut().for_each(|m| {
+                    if *m.1 == right_circuit {
+                        *m.1 = left_circuit;
+                    }
+                });
+            }
+            (Some(left_circuit), None) => {
+                circuits.insert(right, *left_circuit);
+            }
+            (None, Some(right_circuit)) => {
+                circuits.insert(left, *right_circuit);
+            }
+            (None, None) => {
+                circuits.insert(left, idx);
+                circuits.insert(right, idx);
+                idx += 1;
+            }
+        }
+
+        if circuits.len() == input.len() && circuits.values().unique().count() == 1 {
+            return Some((left[0] * right[0]) as u64);
+        }
+    }
+
     None
 }
 
@@ -120,6 +172,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(25272));
     }
 }
